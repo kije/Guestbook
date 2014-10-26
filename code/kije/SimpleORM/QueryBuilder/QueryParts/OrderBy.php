78c2282432d6ef02e554daf413d02214afdcb41b
Kim Jeker <info@kije.ch>
2014-10-26 11:56:09 +0100

    Implemented OrderBy

--- code/kije/SimpleORM/QueryBuilder/QueryParts/OrderBy.php
+++ code/kije/SimpleORM/QueryBuilder/QueryParts/OrderBy.php
@@ -10,12 +10,36 @@ namespace kije\SimpleORM\QueryBuilder\QueryParts;
  */
 class OrderBy extends AbstractQueryPart
 {
+    protected $columns;
+
+    public function add($col, $desc = false) {
+        $this->columns[$col] = $desc ? 'DESC' : 'ASC';
+
+        return $this;
+    }
 
     /**
      * @return string
      */
     public function getSql()
     {
-        // TODO: Implement getSql() method.
+        if (!empty($this->columns)) {
+            $sql = ' ORDER BY ';
+
+            $i = 0;
+            $count = count($this->columns);
+            foreach($this->columns as $col => $order) {
+                $i++;
+                $sql .= '`'.$col.'` '.$order;
+
+                if ($i > $count) {
+                    $sql .= ',';
+                }
+            }
+
+            return $sql;
+        }
+
+        return false;
     }
 }
