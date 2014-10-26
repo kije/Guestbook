46174b7fcd92551060f2e6b547b5cfa4177ffab1
Kim Jeker <info@kije.ch>
2014-10-26 12:01:39 +0100

    Added possibility to Filters to run code if the check was successful or has failed

--- code/kije/Guestbook/Filters/Filter.php
+++ code/kije/Guestbook/Filters/Filter.php
@@ -12,15 +12,41 @@ namespace kije\Guestbook\Filters;
  * @package kije\Guestbook\Filters
  * @see \kije\Routing\Filter
  */
-abstract class Filter extends \kije\Routing\Filter {
+abstract class Filter extends \kije\Routing\Filter
+{
 
     /**
      * @param callable|\kije\Base\Runnable|\kije\Routing\Route|string $action
      * @param null|mixed $action_params
      */
-    public function __construct($action, $action_params = null) {
-        parent::__construct(array($this, 'check'), $action, $action_params);
+    public function __construct($action, $action_params = null)
+    {
+        parent::__construct(array($this, '_checkWrapper'), $action, $action_params);
+    }
+
+    protected function _checkWrapper()
+    {
+        $res = $this->check();
+
+        if ($res) {
+            $this->onSuccess();
+        } else {
+            $this->onFail();
+        }
+
+        return $res;
     }
 
     public abstract function check();
-} 
+
+
+    protected function onSuccess()
+    {
+
+    }
+
+    protected function onFail()
+    {
+
+    }
+}
