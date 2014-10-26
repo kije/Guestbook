f03542f469f4c1a597b6b527c972fb409b7722a2
Kim Jeker <info@kije.ch>
2014-10-26 12:09:42 +0100

    Added routes

--- code/kije/Guestbook/routes.php
+++ code/kije/Guestbook/routes.php
@@ -6,12 +6,16 @@
  */
 namespace kije\Guestbook;
 
-use kije\Guestbook\Routes\LoginRoute;
-use kije\Guestbook\Routes\LogoutRoute;
-use kije\Guestbook\Routes\OverviewRoute;
+use kije\Guestbook\Routes\Create;
+use kije\Guestbook\Routes\Login;
+use kije\Guestbook\Routes\Logout;
+use kije\Guestbook\Routes\Overview;
+use kije\Guestbook\Routes\Register;
 use kije\Routing\RouteHandler;
 
 
-RouteHandler::registerRoute(new LoginRoute('/login', array('/l', '/log-in'), true));
-RouteHandler::registerRoute(new LogoutRoute('/logout'));
-RouteHandler::registerRoute(new OverviewRoute());
+RouteHandler::registerRoute(new Login('/login', array('/l', '/log-in'), true));
+RouteHandler::registerRoute(new Logout('/logout', array('/log-out'), true));
+RouteHandler::registerRoute(new Register('/register', array('/r'), true));
+RouteHandler::registerRoute(new Overview('/', array('/overview', '/o'), false));
+RouteHandler::registerRoute(new Create('/create', array('/post/create', '/c'), true));
